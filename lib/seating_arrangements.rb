5b3a3cdf493830e23e1f07fa636a8b4192c0aa2f
Michael King <kingmt@gmail.com>
2017-03-18 15:24:41 -0500

    add library checking table, autoplace and can_be_deleted

---
+++ lib/seating_arrangements.rb
@@ -0,0 +1,56 @@
+require 'seating_rules'
+# built as a module since the logic doesn't truely belong
+# in the models
+
+module SeatingArrangements
+  extend self
+
+  # receives an array of seats
+  # returns an array of errors
+  def check_table seats
+    errors = []
+    seats.each_with_index do |seat, index|
+      left = if seat == seats.first
+               seats.last
+             else
+               seats[index - 1]
+             end
+      right = if seat == seats.last
+                seats.first
+              else
+                seats[index + 1]
+              end
+      errors << SeatingRules.check_all_rules(left.person, seat.person, right.person)
+    end
+    errors.flatten.compact
+  end
+
+  # given a list of seats and a person to add
+  # attempt to place the new seat
+  # returns a new list of seats if successful
+  # returns nil if unsuccessful
+  def autoplace seats, new_peep
+    # brute force, naive implementation
+    new_seat = Seat.new table: (seats.first.table), person: new_peep
+    result = seats.each_index do |index|
+               new_list = seats.dup
+               new_list.insert index, new_seat
+               if check_table(new_list).empty?
+                 break new_list
+               end
+             end
+    if result == seats
+      nil
+    else
+      result
+    end
+  end
+
+  # given a list of seats and which one to delete
+  # returns true if the seat can be deleted leaving a valid table
+  def can_be_deleted seats, to_be_deleted
+    new_list = seats.dup
+    new_list.delete to_be_deleted
+    check_table(new_list).empty?
+  end
+end
---
+++ spec/lib/seating_arrangements_spec.rb
@@ -0,0 +1,114 @@
+require 'rails_helper'
+require 'seating_arrangements'
+
+RSpec.describe SeatingArrangements do
+  let(:table) { Table.create }
+
+  let(:sam)     { Person.create name: 'Sam',     age: 22 }
+  let(:john)    { Person.create name: 'John',    age: 30 }
+  let(:chris)   { Person.create name: 'Chris',   age: 21 }
+  let(:matt)    { Person.create name: 'Matt',    age: 25 }
+  let(:michael) { Person.create name: 'Michael', age: 19 }
+  let(:sara)    { Person.create name: 'Sara',    age: 19 }
+
+  let(:sam_seat)     { Seat.create person: sam,     table: table }
+  let(:john_seat)    { Seat.create person: john,    table: table }
+  let(:chris_seat)   { Seat.create person: chris,   table: table }
+  let(:matt_seat)    { Seat.create person: matt,    table: table }
+  let(:michael_seat) { Seat.create person: michael, table: table }
+  let(:sara_seat)    { Seat.create person: sara,    table: table }
+
+  describe 'check_table' do
+    context 'returns empty errors' do
+      it 'for single seat' do
+        seats = [michael_seat]
+        expected = []
+        result = SeatingArrangements.check_table seats
+        expect(result).to eq expected
+      end
+
+      it 'for 2 seats' do
+        seats = [michael_seat, sara_seat]
+        expected = []
+        result = SeatingArrangements.check_table seats
+        expect(result).to eq expected
+      end
+
+      it 'for more seats' do
+        seats = [michael_seat, sara_seat, sam_seat, chris_seat]
+        expected = []
+        result = SeatingArrangements.check_table seats
+        expect(result).to eq expected
+      end
+    end
+
+    it 'returns 1 error' do
+      seats = [michael_seat, sam_seat, matt_seat, chris_seat]
+      expected = ["Michael is younger than both Chris and Sam"]
+      result = SeatingArrangements.check_table seats
+      expect(result).to eq expected
+    end
+
+    it 'returns a mess of errors' do
+      seats = [michael_seat, sam_seat, sara_seat, john_seat, chris_seat]
+      expected = ["Michael is younger than both Chris and Sam",
+                  "The age difference between John and Sara is greater than 5",
+                  "Sara is younger than both Sam and John",
+                  "The age difference between Sara and John is greater than 5",
+                  "The age difference between Chris and John is greater than 5",
+                  "The age difference between John and Chris is greater than 5"
+                 ]
+      result = SeatingArrangements.check_table seats
+      # Have to break all the checks apart since rspec is abreviating the diff
+      expect(result.size).to eq expected.size
+      expect(result[0]).to eq expected[0]
+      expect(result[1]).to eq expected[1]
+      expect(result[2]).to eq expected[2]
+      expect(result[3]).to eq expected[3]
+      expect(result[4]).to eq expected[4]
+      expect(result[5]).to eq expected[5]
+    end
+  end
+
+  describe 'autoplace' do
+    it 'places the new person at the front' do
+      seats = [michael_seat, sara_seat, chris_seat]
+      result = SeatingArrangements.autoplace seats, sam
+      # need to match internals since any new seat object created
+      # here won't be the same seat object created in autoplace
+      expect(result[0].person).to eq sam
+      # remainder of seat list is unchanges
+      expect(result.slice(1,3)).to eq seats
+    end
+
+    it 'places the new person in the middle' do
+      seats = [michael_seat, sara_seat, sam_seat, chris_seat]
+      result = SeatingArrangements.autoplace seats, matt
+      # expect Matt to be between Sam and Chris
+      # so first 3 seats are unchanged
+      expect(result.slice(0,3)).to eq seats.slice(0,3)
+      expect(result.last).to eq seats.last
+      expect(result[3].person).to eq matt
+    end
+
+    it 'cannot place the new person' do
+      seats = [michael_seat, sara_seat, chris_seat]
+      result = SeatingArrangements.autoplace seats, john
+      expect(result).to eq nil
+    end
+  end
+
+  describe 'can_be_deleted' do
+    it 'returns true' do
+      seats = [michael_seat, sara_seat, sam_seat, chris_seat]
+      result = SeatingArrangements.can_be_deleted seats, sam_seat
+      expect(result).to eq true
+    end
+
+    it 'returns false' do
+      seats = [michael_seat, sara_seat, sam_seat, chris_seat]
+      result = SeatingArrangements.can_be_deleted seats, sara_seat
+      expect(result).to eq false
+    end
+  end
+end
