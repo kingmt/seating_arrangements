03a62e74fda534ba4788b77e9a5ae7759f1cee69
Michael King <kingmt@gmail.com>
2017-03-18 21:12:42 -0500

    add delegates

--- spec/models/seat_spec.rb
+++ spec/models/seat_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Seat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'without a person' do
+    let(:seat) { Seat.new }
+
+    it 'delegates the name' do
+      expect(seat.name).to eq nil
+    end
+
+    it 'delegates the age' do
+      expect(seat.age).to eq nil
+    end
+  end
+
+  context 'with a person' do
+    let(:person) { create :person }
+    let(:seat) { Seat.new person: person }
+
+    it 'delegates the name' do
+      expect(seat.name).to eq person.name
+    end
+
+    it 'delegates the age' do
+      expect(seat.age).to eq person.age
+    end
+  end
+
+
 end
