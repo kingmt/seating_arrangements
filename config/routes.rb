0b81bf5ebd80fa4e3ef7ec6196552e1312ec0cf7
Michael King <kingmt@gmail.com>
2017-03-19 11:28:30 -0500

    missed show action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resources :tables, only: [:create, :update, :index, :show, :destroy] do
       resources :seats, only: [:create, :update, :destroy]
     end
-    resources :people, only: [:create, :index, :update, :destroy]
+    resources :people, only: [:create, :index, :show, :update, :destroy]
   end
 end
