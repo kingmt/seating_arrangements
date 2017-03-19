3b0329f219ce912fdde869433d94fda0af27aab3
Michael King <kingmt@gmail.com>
2017-03-18 21:12:23 -0500

    add delegates

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,4 +1,26 @@
 class Seat < ApplicationRecord
   belongs_to :table
   belongs_to :person
+
+  acts_as_list scope: :table
+
+  def name
+    if person
+      person.name
+    else
+      nil
+    end
+  end
+
+  def age
+    if person
+      person.age
+    else
+      nil
+    end
+  end
+
+  def can_be_unseated
+    TableRules.can_be_unseated table.seats.to_a, self
+  end
 end
